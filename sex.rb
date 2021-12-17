7a9a4e1a219bc178b8b8c64521982185b53ccf7f
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-18 01:18:01 +0200

    Create sex.rb

---
+++ sex.rb
@@ -0,0 +1,14 @@
+# C:\Ruby\lesson_23\sex.rb
+
+#Мужчины
+#["Gena", "George", "Misha"]
+#Женщины
+#["Katrin", "Liza", "Masha"]
+#Все вместе
+#["Gena", "George", "Misha", "Katrin", "Liza", "Masha"]
+
+men = ["Gena", "George", "Misha"]
+women = ["Katrin", "Liza", "Masha"]
+all = men + women
+
+puts all.to_s
