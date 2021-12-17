3558584448d322162bf53180ef69f505faa97e03
Vira <74150565+Vira1607@users.noreply.github.com>
2021-12-18 01:17:33 +0200

    Create road.rb

---
+++ road.rb
@@ -0,0 +1,17 @@
+# C:\Ruby\lesson_22\road.rb
+
+puts "Start"
+sleep 1
+
+puts "Первый километр"
+sleep 1
+
+puts "Второй километр"
+sleep 1
+
+puts "Третий километр"
+sleep 1
+
+puts "Finish!"
+
+gets
